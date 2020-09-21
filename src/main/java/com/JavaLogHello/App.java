3b2f4f9842944f4dc585a86e9fdb55a9d4483b78
Aditya Verma <aaadarths2357@gmail.com>
2020-09-21 13:49:16 +0530

    All files Added Successfully

---
+++ src/main/java/com/JavaLogHello/App.java
@@ -0,0 +1,23 @@
+package com.JavaLogHello;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+ 
+
+public class App 
+{
+	private static final Logger Log=LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	
+    	String message="Hello World!!!";
+    	Log.debug(message+" Debug Message");
+    	Log.info(message+" Info Message");
+    	Log.warn(message+" Warn Message");
+    	Log.error(message+" Error Message");
+    	Log.fatal(message+" Fatal Message");
+        System.out.println( message );
+    }
+}
